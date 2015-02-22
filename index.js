345b6594580656aecb2c19a60ae4e5dd0094c082
Cédric <cedced19@gmail.com>
2015-02-22 10:34:22 +0100

    Don't skip line

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 var latest = require('./lib/latest-version.js'), colors = require('colors');
 var defaultMessage = function (options) {
   if (options.latestVersion == options.currentVersion) {
-    return '\nYou have the latest version of ' + colors.green(options.name) + colors.dim(' (current: ' + options.currentVersion + ')');
+    return 'You have the latest version of ' + colors.green(options.name) + colors.dim(' (current: ' + options.currentVersion + ')');
   } else {
-    return '\nUpdate available: ' + colors.green(options.latestVersion) + colors.dim(' (current: ' + options.currentVersion + ')');
+    return 'Update available: ' + colors.green(options.latestVersion) + colors.dim(' (current: ' + options.currentVersion + ')');
   }
 };
 module.exports = function (options, cb) {
