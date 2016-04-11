65d8995dabc0af8c01cbc0058bac7bfc11a58b8f
Cédric <cedced19@gmail.com>
2016-04-11 13:39:45 +0200

    Fix a problem with detection of undifined variables

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 'use strict';
 var checkUpdate = require('./');
 describe('checkUpdate', function () {
-    
+
   it('should check for update and display true', function () {
     checkUpdate({
       name: 'check-update-tester',
@@ -14,7 +14,19 @@ describe('checkUpdate', function () {
       }
     });
   });
-    
+
+  it('should check for update and display true without any branch defined', function () {
+    checkUpdate({
+      name: 'check-update-tester',
+      currentVersion: '0.0.1',
+      user: 'cedced19',
+      branch: 'master'
+    }, function (err, lastestVersion, defaultMessage) {
+      if (!err) {
+        console.log(defaultMessage);
+      }
+    });
+  });
 
   it('should check for update and display false', function () {
     checkUpdate({
@@ -28,7 +40,7 @@ describe('checkUpdate', function () {
       }
     });
   });
-  
+
   it('should check for update and display the version', function () {
     checkUpdate({
       name: 'check-update-tester',
